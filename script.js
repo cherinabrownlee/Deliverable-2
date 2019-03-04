168c4b9d0053675efc788ce05107c1ccdeb6f84f
cherinabrownlee <48037027+cherinabrownlee@users.noreply.github.com>
2019-03-04 03:25:08 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+if (eventType) === "casual" && temperature > 54) {
+  para.textContent = "It is" + temperature. "You should wear something comfy and a coat";
+} else if (eventType) === "semi-formal" && temperature < 54) {
+  para.textContent = "It is" && temperature. "You should wear a polo and a jacket";
+} else 
+  "You should wear a suit and no jacket";
+}
